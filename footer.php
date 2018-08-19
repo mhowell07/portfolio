ad9c5f64616e81169c69fab455c8207bd9ac82dd
Matt Howell <matthew.howell07@gmail.com>
2018-08-19 02:44:55 +0000

    Created base WordPress theme

---
+++ footer.php
@@ -0,0 +1,17 @@
+  <footer>
+    <div class="container">
+      <div clas="row">
+        <div class="col-sm-12 text-center white">
+          All rights reserved &copy;<?php echo date("Y"); ?> | Matt Howell
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Footer Scripts -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="referrer" content="origin">
+
+    <title>Matt Howell Portfolio</title>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link rel="stylesheet" href="/wp-content/themes/matt-howell/style.css">
+
+    <!-- Header Scripts -->
+
+    <?php wp_head(); ?>
+
+  </head>
+
+  <body>
